1f8fcda56d90fc96e1311731843a8257665ed63a
yanhongxu <yanhx9558@gmail.com>
2020-06-03 13:16:50 +0800

    fix bug in proj1b

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -12,5 +12,9 @@ public class TestOffByOne {
     public void testEqualChars() {
         assertTrue(offByOne.equalChars('a', 'b'));
         assertTrue(offByOne.equalChars('r', 'q'));
+        assertFalse(offByOne.equalChars('a', 'e'));
+        assertFalse(offByOne.equalChars('z', 'a'));
+        assertFalse(offByOne.equalChars('a', 'a'));
+        assertTrue(offByOne.equalChars('&', '%'));
     }
 }
