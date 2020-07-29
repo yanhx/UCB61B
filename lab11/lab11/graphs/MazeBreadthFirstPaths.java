2c80a30a8eb093910efc8f2859c975cc8fea462d
yanhongxu <yanhx9558@gmail.com>
2020-07-29 12:02:07 +0800

    finished lab11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,7 +1,9 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Queue;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeBreadthFirstPaths extends MazeExplorer {
     /* Inherits public fields:
@@ -9,21 +11,52 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s, t;
+    private boolean targetFound = false;
+    private Maze maze;
+    private Queue<Integer> q;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        q = new Queue<>();
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
-    /** Conducts a breadth first search of the maze starting at the source. */
+    /**
+     * Conducts a breadth first search of the maze starting at the source.
+     */
     private void bfs() {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        q.enqueue(s);
+        marked[s] = true;
+        announce();
+        while (!q.isEmpty()) {
+            int v = q.dequeue();
+            if (v == t) {
+                targetFound = true;
+                return;
+            }
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    announce();
+                    q.enqueue(w);
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,7 +1,7 @@
 package lab11.graphs;
 
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeCycles extends MazeExplorer {
     /* Inherits public fields:
@@ -9,14 +9,50 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private boolean cycleFound = false;
+    private Maze maze;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+    }
+
+    private int dfs(int v) {
+        marked[v] = true;
+        announce();
+        for (int w : maze.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                //distTo[w] = distTo[v] + 1;
+                announce();
+                int tmp = dfs(w);
+                if (tmp == -1) {
+                    edgeTo[w] = Integer.MAX_VALUE;
+                    //distTo[w] = Integer.MAX_VALUE;
+                    announce();
+                }
+                if (cycleFound) {
+                    if (tmp == w) {
+                        return -1;
+                    } else {
+                        return tmp;
+                    }
+                }
+            } else {
+                if (edgeTo[v] != w) {
+                    cycleFound = true;
+                    edgeTo[w] = v;
+                    announce();
+                    return w;
+                }
+            }
+        }
+        return -1;
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfs(0);
     }
 
     // Helper methods go here
